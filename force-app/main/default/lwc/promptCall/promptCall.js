c71888a1ea6a4c0efae2342c8c9c0913b7f8b8aa
Thomas Braad Perret-Gentil <github@magicx.dk>
2024-02-27 08:35:34 +0000

    FirstCommit

---
+++ force-app/main/default/lwc/promptCall/promptCall.js
@@ -0,0 +1,26 @@
+import { LightningElement,track,api } from 'lwc';
+import generateResponse from "@salesforce/apex/PromptCallGA.generateResponse";
+import { getRecord } from "lightning/uiRecordApi";
+
+export default class PromptCall extends LightningElement {
+
+    @api recordId;
+    @track promptResponse;
+    @track isLoading = false;
+
+    callPrompt()
+    {
+        console.debug("Calling prompt...");
+        this.isLoading = true;
+        generateResponse({ messageId: this.recordId }).then((result) => {
+            console.debug("Prompt result: " + result);
+            this.promptResponse = result;
+            this.isLoading = false;
+        }).catch((error) => {
+            console.debug("Prompt call error: " + JSON.stringify(error));
+            this.isLoading = false;
+        });
+
+    }
+
+}
